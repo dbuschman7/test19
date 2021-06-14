1161df9279cf5239e7eb2cf625b92a4a1e14c5e6
David Buschman <dave.buschman@qalocate.com>
2021-06-13 23:43:11 -0500

    renamed Hello class and file from Spec to Suite

--- src/test/scala/example/HelloSuite.scala
+++ src/test/scala/example/HelloSuite.scala
@@ -3,7 +3,7 @@ package example
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 
-class HelloSpec extends AnyFunSuite with Matchers {
+class HelloSuite extends AnyFunSuite with Matchers {
   test("say hello") {
     Hello.greeting mustBe "hello"
   }
